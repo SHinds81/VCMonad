ec2847dc465402356adf627c78c99805ce84acf9
SHinds <seanhinds81@gmail.com>
2020-02-26 12:00:38 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# VCMonad
+ Artwork generation for VC: "Monad"
---
+++ src/application/JavafxSample.java
@@ -0,0 +1,239 @@
+package application;
+
+import javafx.application.Application; 
+import javafx.scene.Group; 
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage; 
+import javafx.scene.shape.Rectangle;
+
+public class JavafxSample extends Application { 
+   @Override 
+   public void start(Stage stage) { 
+	   
+	   /*
+	   System.out.println("Test");
+	   Scanner keyInput = new Scanner(System.in);
+	   String test = keyInput.nextLine();
+	   System.out.println(test);
+	   */
+	   //1600 by 1600 pixels for cd booklet
+	   
+	  int aeon = 20;
+	  aeon = (aeon * aeon) + aeon;
+	  int omega = aeon;
+	  System.out.println("Aeon test: " + aeon);
+	  
+	  Rectangle archonDemiurge = new Rectangle();
+	  Rectangle godDemiurge = new Rectangle();
+	  Rectangle demonDemiurge = new Rectangle();
+	  Rectangle alphaDemiurge = new Rectangle();
+	  Rectangle betaDemiurge = new Rectangle();
+	  Rectangle rhoDemiurge = new Rectangle();
+	  Rectangle sigmaDemiurge = new Rectangle();
+	  Rectangle xiDemiurge = new Rectangle();
+	  Rectangle ultimateDemiurge = new Rectangle();
+	  
+	  Rectangle archonMonad = new Rectangle();
+	  Rectangle godMonad = new Rectangle();
+	  Rectangle demonMonad = new Rectangle();
+	  Rectangle alphaMonad = new Rectangle();
+	  Rectangle betaMonad = new Rectangle();
+	  Rectangle rhoMonad = new Rectangle();
+	  Rectangle sigmaMonad = new Rectangle();
+	  Rectangle xiMonad = new Rectangle();
+	  Rectangle ultimateMonad = new Rectangle();
+      
+
+      
+      int[] aeonOne;
+      aeonOne = new int[9];
+      
+      for (int creator = 0; creator < 9; creator++) {
+    	  aeon = aeon / 2;
+    	  aeonOne[creator] = aeon;
+    	  System.out.println("At this stage (" + creator + ") aeon is at: " + aeonOne[creator]);
+      }
+      
+      //left
+      int demiurgeArchon = 0;
+      archonDemiurge.setX(demiurgeArchon); 
+      archonDemiurge.setY(0.0f); 
+      archonDemiurge.setWidth(aeonOne[0]); 
+      archonDemiurge.setHeight(omega);      
+      archonDemiurge.setFill(Color.BLACK);
+      //right
+      int monadArchon = omega - aeonOne[0];
+      archonMonad.setX(monadArchon); 
+      archonMonad.setY(0.0f); 
+      archonMonad.setWidth(aeonOne[0]); 
+      archonMonad.setHeight(omega);      
+      archonMonad.setFill(Color.WHITE);
+      
+      //left2
+      int demiurgeGod = demiurgeArchon + aeonOne[1];
+      godDemiurge.setX(demiurgeGod); 
+      godDemiurge.setY(0.0f); 
+      godDemiurge.setWidth(aeonOne[1]); 
+      godDemiurge.setHeight(omega);      
+      godDemiurge.setFill(Color.WHITE);
+      //right2
+      int monadGod = monadArchon - aeonOne[1];
+      godMonad.setX(monadArchon); 
+      godMonad.setY(0.0f); 
+      godMonad.setWidth(aeonOne[1]); 
+      godMonad.setHeight(omega);      
+      godMonad.setFill(Color.BLACK);
+      
+      //left3
+      int demiurgeDemon = demiurgeGod + aeonOne[2];
+      demonDemiurge.setX(demiurgeDemon); 
+      demonDemiurge.setY(0.0f); 
+      demonDemiurge.setWidth(aeonOne[2]); 
+      demonDemiurge.setHeight(omega);      
+      demonDemiurge.setFill(Color.BLACK);
+      //right3
+      int monadDemon = monadGod - aeonOne[2];
+      demonMonad.setX(monadArchon); 
+      demonMonad.setY(0.0f); 
+      demonMonad.setWidth(aeonOne[2]); 
+      demonMonad.setHeight(omega);      
+      demonMonad.setFill(Color.WHITE);
+      
+      //left4
+      int demiurgeAlpha = demiurgeDemon + aeonOne[3];
+      alphaDemiurge.setX(demiurgeAlpha); 
+      alphaDemiurge.setY(0.0f); 
+      alphaDemiurge.setWidth(aeonOne[3]); 
+      alphaDemiurge.setHeight(omega);      
+      alphaDemiurge.setFill(Color.WHITE);
+      //right4
+      int monadAlpha = monadGod - aeonOne[3];
+      alphaMonad.setX(monadArchon); 
+      alphaMonad.setY(0.0f); 
+      alphaMonad.setWidth(aeonOne[3]); 
+      alphaMonad.setHeight(omega);      
+      alphaMonad.setFill(Color.BLACK);
+      
+    //left5
+      int demiurgeBeta = demiurgeAlpha + aeonOne[4];
+      betaDemiurge.setX(demiurgeBeta); 
+      betaDemiurge.setY(0.0f); 
+      betaDemiurge.setWidth(aeonOne[4]); 
+      betaDemiurge.setHeight(omega);      
+      betaDemiurge.setFill(Color.BLACK);
+      //right5
+      int monadBeta = monadGod - aeonOne[4];
+      betaMonad.setX(monadArchon); 
+      betaMonad.setY(0.0f); 
+      betaMonad.setWidth(aeonOne[4]); 
+      betaMonad.setHeight(omega);      
+      betaMonad.setFill(Color.WHITE);
+      
+      //left6
+      int demiurgeRho = demiurgeBeta + aeonOne[5];
+      rhoDemiurge.setX(demiurgeRho); 
+      rhoDemiurge.setY(0.0f); 
+      rhoDemiurge.setWidth(aeonOne[5]); 
+      rhoDemiurge.setHeight(omega);      
+      rhoDemiurge.setFill(Color.WHITE);
+      //right6
+      int monadRho = monadGod - aeonOne[5];
+      rhoMonad.setX(monadArchon); 
+      rhoMonad.setY(0.0f); 
+      rhoMonad.setWidth(aeonOne[5]); 
+      rhoMonad.setHeight(omega);      
+      rhoMonad.setFill(Color.BLACK);
+      
+      //left7
+      int demiurgeSigma = demiurgeRho + aeonOne[6];
+      sigmaDemiurge.setX(demiurgeSigma); 
+      sigmaDemiurge.setY(0.0f); 
+      sigmaDemiurge.setWidth(aeonOne[6]); 
+      sigmaDemiurge.setHeight(omega);      
+      sigmaDemiurge.setFill(Color.BLACK);
+      //right7
+      int monadSigma = monadGod - aeonOne[6];
+      sigmaMonad.setX(monadArchon); 
+      sigmaMonad.setY(0.0f); 
+      sigmaMonad.setWidth(aeonOne[6]); 
+      sigmaMonad.setHeight(omega);      
+      sigmaMonad.setFill(Color.WHITE);
+      
+    //left8
+      int demiurgeXi = demiurgeSigma + aeonOne[7];
+      xiDemiurge.setX(demiurgeXi); 
+      xiDemiurge.setY(0.0f); 
+      xiDemiurge.setWidth(aeonOne[7]); 
+      xiDemiurge.setHeight(omega);      
+      xiDemiurge.setFill(Color.WHITE);
+      //right8
+      int monadXi = monadGod - aeonOne[7];
+      xiMonad.setX(monadArchon); 
+      xiMonad.setY(0.0f); 
+      xiMonad.setWidth(aeonOne[7]); 
+      xiMonad.setHeight(omega);      
+      xiMonad.setFill(Color.BLACK);
+      
+      //left9
+      int demiurgeUlimate = demiurgeXi + aeonOne[8];
+      ultimateDemiurge.setX(demiurgeUlimate); 
+      ultimateDemiurge.setY(0.0f); 
+      ultimateDemiurge.setWidth(aeonOne[8]); 
+      ultimateDemiurge.setHeight(omega);      
+      ultimateDemiurge.setFill(Color.BLACK);
+      //right9
+      int monadUlimate = monadGod - aeonOne[8];
+      ultimateMonad.setX(monadArchon); 
+      ultimateMonad.setY(0.0f); 
+      ultimateMonad.setWidth(aeonOne[8]); 
+      ultimateMonad.setHeight(omega);      
+      ultimateMonad.setFill(Color.WHITE);
+      
+            
+      //Creating a Group object  
+      Group root = new Group(
+    		  archonDemiurge, 
+    		  archonMonad, 
+    		  
+    		  godDemiurge, 
+    		  godMonad, 
+    		  
+    		  demonDemiurge, 
+    		  demonMonad, 
+    		  
+    		  alphaDemiurge, 
+    		  alphaMonad, 
+    		  
+    		  betaDemiurge, 
+    		  betaMonad, 
+    		  
+    		  rhoDemiurge, 
+    		  rhoMonad, 
+    		  
+    		  sigmaDemiurge, 
+    		  sigmaMonad, 
+    		  
+    		  xiDemiurge, 
+    		  xiMonad, 
+    		  
+    		  ultimateDemiurge, 
+    		  ultimateMonad);
+    		 
+         
+      //Creating a scene object 
+      Scene scene = new Scene(root, omega, omega);  
+      
+      //Setting title to the Stage 
+      stage.setTitle("Voidchrist: Monad"); 
+         
+      //Adding scene to the stage 
+      stage.setScene(scene); 
+         
+      //Displaying the contents of the stage 
+      stage.show(); 
+   }      
+   public static void main(String args[]){ 
+      launch(args); 
+   } 
+} 
